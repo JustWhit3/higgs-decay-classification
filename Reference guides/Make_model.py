98fe68985e076a8c64b50029af73103c66bcb17c
JustWhit3 <48323961+JustWhit3@users.noreply.github.com>
2020-04-01 11:03:13 +0200

    Update Make_model.py
    Added "keras" libraries.

--- Reference guides/Make_model.py
+++ Reference guides/Make_model.py
@@ -1,3 +1,8 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.regularizers import l1, l2
+from keras.callbacks.callbacks import EarlyStopping
+
 def make_model(layer_sizes, activation, dropout_rate, optimizer, regularization, lambda_reg, input_dimension):
     '''Creates model comprised of dense layers'''
     model = Sequential()
