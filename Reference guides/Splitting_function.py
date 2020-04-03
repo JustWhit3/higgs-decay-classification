a85390a1729b6f74530f3ac3770b0e9816fd0282
JustWhit3 <48323961+JustWhit3@users.noreply.github.com>
2020-04-03 12:51:17 +0200

    Update Splitting_function.py
    Added testing to the "splitting" function.

--- Reference guides/Splitting_function.py
+++ Reference guides/Splitting_function.py
@@ -53,3 +53,31 @@ def splitting (dataset, string, thing):
     else: None
         
     del subset
+
+    return 0
+
+#TESTING "SPLITTING" FUNCTION:
+from hypothesis import given, settings
+import hypothesis.strategies as st
+import hypothesis.extra
+from hypothesis.extra.numpy import arrays
+from hypothesis.extra.pandas import series, range_indexes, columns, data_frames
+
+@given(
+       x=data_frames(columns=columns(["EventId", "DER_mass_MMC", "DER_mass_transverse_met_lep",
+       'DER_mass_vis', 'DER_pt_h', 'DER_deltaeta_jet_jet', 'DER_mass_jet_jet',
+       'DER_prodeta_jet_jet', 'DER_deltar_tau_lep', 'DER_pt_tot', 'DER_sum_pt',
+       'DER_pt_ratio_lep_tau', 'DER_met_phi_centrality',
+       'DER_lep_eta_centrality', 'PRI_tau_pt', 'PRI_tau_eta', 'PRI_tau_phi',
+       'PRI_lep_pt', 'PRI_lep_eta', 'PRI_lep_phi', 'PRI_met', 'PRI_met_phi',
+       'PRI_met_sumet', 'PRI_jet_num', 'PRI_jet_leading_pt',
+       'PRI_jet_leading_eta', 'PRI_jet_leading_phi', 'PRI_jet_subleading_pt',
+       'PRI_jet_subleading_eta', 'PRI_jet_subleading_phi', 'PRI_jet_all_pt',
+       'Weight', 'Label', 'KaggleSet', 'KaggleWeight'],
+       dtype=float),
+       rows=st.tuples(st.floats(allow_nan=False), st.floats(allow_nan=False)).map(sorted)),
+       y=st.text(),
+       z=st.text(), 
+      )
+def test_splitting(x,y,z):
+    assert splitting(x,y,z) == 0
