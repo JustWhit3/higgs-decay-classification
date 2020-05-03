23ffed27daaf6301cfeb4539d86cb4bd4b751c59
JustWhit3 <48323961+JustWhit3@users.noreply.github.com>
2020-05-03 09:39:30 +0200

    Update Plot_distributions.py
    Updates:
    1) Removed unuseful testing for plotting functions.
    2) Update of the documentation.
    3) Added spaces between function definitions.
    4) Removed the unuseful "return 0" of each function.

--- Reference guides/Plot_distributions.py
+++ Reference guides/Plot_distributions.py
@@ -1,18 +1,20 @@
-#There are two functions defined in my program: `plot_distributions` and `plot_distributions_final`.This functions are useful for the plotting of the distributions of each model. I'll explain only the second one, because it's more complete and extended in respect to the first one. So, this one takes 7 arguments:
+#There are two important plotting functions defined in my program: `plot_distributions` and `plot_distributions_final`. This functions are useful for the plotting of the distributions of each model. 
+#I'll show the explanation of only the second one, because it's more complete and extended in respect to the first one. So, this one takes 7 arguments:
 
-#+ "prediction_val" that are prediction data for the validation set. It's a 2-dim array.
-#+ "prediction_test" that are prediction data for the test set. It's a 2-dim array.
-#+ "true_val" that are the output data of the model (for the validation set). It's a 2-dim array.
-#+ "n_bins" that are the number of bins (usually set to 50). It's an integer.
-#+ "weighted" is a boolean variable set to be True if the histogram is weighted, otherwise if it's unweighted.
-#+ "weights_val" in case in which my histogram is weighted this are the weights of the validation data.
-#+ "weights_test" and this are the weights of the test data.
+#1) "prediction_val" that are prediction data for the validation set. It's a 2-dim array.
+#2) "prediction_test" that are prediction data for the test set. It's a 2-dim array.
+#3) "true_val" that are the output data of the model (for the validation set). It's a 2-dim array.
+#4) "n_bins" that are the number of bins (usually set to 50). It's an integer.
+#5) "weighted" is a boolean variable set to be True if the histogram is weighted, otherwise if it's unweighted.
+#6) "weights_val" in case in which my histogram is weighted this are the weights of the validation data.
+#7) "weights_test" and this are the weights of the test data.
 
 import numpy as np
 from matplotlib import pyplot as plt
 
 def plot_distributions_final(prediction_val, prediction_test, true_val, n_bins, weighted, weights_val, weights_test):
     '''Useful to plot the final result, considering also the test set.'''
+
     # Get histograms from our model
     if weighted:
         hist_b = np.histogram(prediction_val[:,1][true_val[:,0]==1], bins=n_bins, range=(0,1), 
@@ -52,7 +54,6 @@ def plot_distributions_final(prediction_val, prediction_test, true_val, n_bins, 
     plt.ylabel("Counts")
     plt.yscale('log')
 
-    return 0
 
 def plot_distributions(prediction, true, n_bins, weighted, weights):
     '''Useful to plot the final result, without considering also the test set.'''
@@ -86,42 +87,5 @@ def plot_distributions(prediction, true, n_bins, weighted, weights):
     plt.ylabel("Counts")
     plt.yscale('log')
 
-    return 0
 
-#TESTING "PLOT_DISTRIBUTIONS":
-from hypothesis import given, settings
-import hypothesis.strategies as st
-import hypothesis.extra
-from hypothesis.extra.numpy import arrays
-from hypothesis.extra.pandas import series, range_indexes
 
-@given(
-       x=arrays(np.float64, (1,100000), elements=st.floats(0,1), fill=None, unique=False),
-       y=arrays(np.int8, (1,100000), elements=st.floats(1,1), fill=None, unique=False), 
-       z=st.integers(1,100),
-       t=st.booleans(),
-       k=series(elements=st.floats(1,20), dtype=np.float64, index=range_indexes(min_size=1, max_size=1), fill=None, unique=False)
-      )
-@settings(deadline=None)
-def test_plot_distributions(x,y,z,t,k):
-    assert plot_distributions(x,y,z,t,k) == 0
-
-#TESTING "PLOT_DISTRIBUTIONS_FINAL":
-from hypothesis import given, settings
-import hypothesis.strategies as st
-import hypothesis.extra
-from hypothesis.extra.numpy import arrays
-from hypothesis.extra.pandas import series, range_indexes
-
-@given(
-       x_val=arrays(np.float64, (1,100000), elements=st.floats(0,1), fill=None, unique=False),
-       x_test=arrays(np.float64, (1,100000), elements=st.floats(0,1), fill=None, unique=False),
-       y=arrays(np.int8, (1,100000), elements=st.floats(1,1), fill=None, unique=False), 
-       z=st.integers(1,100),
-       t=st.booleans(),
-       k_val=series(elements=st.floats(1,20), dtype=np.float64, index=range_indexes(min_size=1, max_size=1), fill=None, unique=False),
-       k_test=series(elements=st.floats(1,20), dtype=np.float64, index=range_indexes(min_size=1, max_size=1), fill=None, unique=False)
-      )
-@settings(deadline=None)
-def test_plot_distributions_final(x_val,x_test,y,z,t,k_val,k_test):
-    assert plot_distributions_final(x_val,x_test,y,z,t,k_val,k_test) == 0
