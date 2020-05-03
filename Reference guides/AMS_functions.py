3537146be3d5c49fe8ee676a170fba5039d9e823
JustWhit3 <48323961+JustWhit3@users.noreply.github.com>
2020-05-03 09:27:32 +0200

    Update AMS_functions.py
    Updates:
    1) Update of the function documentation.
    2) Removed unuseful function testing on the plotting function.
    3) Added more spaces after the end of each function.

--- Reference guides/AMS_functions.py
+++ Reference guides/AMS_functions.py
@@ -3,15 +3,16 @@
 
 #1) `NN_output_to_AMS`: 
 #this function takes 4 arguments:
-#+ "x_cut" is the cut parameter of the AMS. It ranges from 0.5 to 1 in steps of 0.1.
-#+ "predictions" is a binary array, defined from the set of data that we're considering (for ex: validation set).
-#+ "label_vectors" is a binary array constructed from the dataset, used for each model, that distinguishes an event between signal and background.
-#+ "weights" this takes the weights associated to each data of my dataset (in my case the "KaggleWeight").
+# 1. "x_cut" is the cut parameter of the AMS. It ranges from 0.5 to 1 in steps of 0.1.
+# 2. "predictions" is a binary array, defined from the set of data that we're considering (for ex: validation set).
+# 3. "label_vectors" is a binary array constructed from the dataset, used for each model, that distinguishes an event between signal and background.
+# 4. "weights" it takes the weights associated to each data of my dataset (in my case the "KaggleWeight").
 
-#2) `plot_AMS`: this function takes similar arguments of the previous one. It uses the previous one to plot the final result of the AMS.
+#2) `plot_AMS`: this function takes similar arguments of the previous one. It uses the previous function to plot the final result of the AMS.
 
 import numpy as np
 from matplotlib import pyplot as plt
+
 
 def NN_output_to_AMS(x_cut, predictions, label_vectors, weights):
     '''Useful to calculate the AMS score.'''
@@ -38,8 +39,6 @@ def plot_AMS(predictions, label_vectors, weights):
     
     print('The best AMS Score is {:.3f} at a Cut Parameter of {:.2f}'.format(max(y), x[np.argmax(y)]))
 
-    return 0
-
 
 #TESTING "NN_OUTPUT_TO_AMS":
 from hypothesis import given, settings
@@ -61,19 +60,6 @@ def test_NN_output_to_AMS(x,y,z,t):
     b = sum(t[(y[:,1] > x)  & (z[:,1] ==0)])
     AMS = np.sqrt(  2 *( (s + b + b_reg) * np.log(1 + s/(b + b_reg)) -s )  )
     assert NN_output_to_AMS(x,y,z,t) == AMS
-
-
-#TESTING "PLOT_AMS":
-@given(
-       y=arrays(np.float64, (1,100000), elements=st.floats(0,1), fill=None, unique=False),
-       z=arrays(np.int8, (1,100000), elements=None, fill=None, unique=False), 
-       t=series(elements=None, dtype=np.float64, index=range_indexes(min_size=1, max_size=1), fill=None, unique=False)
-      )
-@settings(deadline=None)
-def test_plot_AMS(y,z,t):
-    x_p = np.arange(0.5,1,1e-2)
-    y_p = np.array(  [NN_output_to_AMS(x,y,z,t) for x in x_p ]  )
-    assert plot_AMS(y,z,t) == 0
 
 
 
